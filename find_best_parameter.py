45f530966ef813335f681d1585941b5625fea059
Andrey Minin <ewkjf@mail.ru>
2021-11-03 23:44:43 +0300

    making some experiments

--- find_best_parameter.py
+++ find_best_parameter.py
@@ -4,23 +4,11 @@ from EngineSecond import Engine
 def find_best_arg_range():
     proc_am = 8
     items = 26
-    for i in [500]:
+
+    for i in range(100, 179, 3):
         eng = Engine(proc_amount=proc_am, max_depth=items, arg=i)
         eng.run()
         print(f'[*] step with arg={i} is done')
-    # for i in range(450, 2300, 300):
-    #     eng = Engine(proc_amount=proc_am, max_depth=items, arg=i)
-    #     eng.run()
-    #     print(f'[*] step with arg={i} is done')
-
-    # for i in range(100, 179, 3):
-    #     eng = Engine(proc_amount=proc_am, max_depth=items, arg=i)
-    #     eng.run()
-    #     print(f'[*] step with arg={i} is done')
-    # for i in range(180, 301, 2):
-    #     eng = Engine(proc_amount=proc_am, max_depth=items, arg=i)
-    #     eng.run()
-    #     print(f'[*] step with arg={i} is done')
 
 
 if __name__ == '__main__':
